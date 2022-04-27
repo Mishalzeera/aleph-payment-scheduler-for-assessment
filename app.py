3012608f438236aa51ca6b0e3aeafc6b14ebbb23
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-04-27 14:39:19 +0200

    Payment variables sent to template, transactions without error on console - no payments in gateway.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
-import string
 import requests
 import json
-import base64
 from flask import (
     Flask, render_template, request, flash, redirect, session, url_for)
 if os.path.exists("env.py"):
@@ -15,19 +13,58 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route('/')
 def index():
-    url = 'https://afs.gateway.mastercard.com/api/rest/version/63/merchant/TEST100065243/session'
+    return render_template("index.html")
+
+@app.route('/payment')
+def payment():
+
+    # URL to post initial request to 
+    post_url = 'https://afs.gateway.mastercard.com/api/rest/version/63/merchant/TEST100065243/session'
+
+    # auth environment variables
     afs_user = os.environ.get("AFS_USER")
     afs_pass = os.environ.get("AFS_PASS")
-    payload = json.dumps({
+
+    # JSON payload to send in POST request body
+    post_payload = json.dumps({
         "session": {
             "authenticationLimit": 25
             
         }
     })
     
-    res = requests.post(url, data=payload, auth=(afs_user, afs_pass))
-    response = res.json()
-    return render_template("index.html", response=response)
+    # send the POST request and store in "res" variable
+    res = requests.post(post_url, data=post_payload, auth=(afs_user, afs_pass))
+
+    # convert JSON response to Python dict
+    post_response = res.json()
+
+    # extract the session id
+    session_id = post_response["session"]["id"]
+
+    # create put URL by conc'ing the post_url with the session_id 
+    put_url = f'{post_url}/{session_id}'
+
+    # add in the payment amount
+    put_payload = json.dumps({
+    "order":{
+       "amount":.1,
+       "currency":"BHD"
+    }
+ })
+    # update the session with the payment amount
+    put_response = requests.put(put_url, data=put_payload, auth=(afs_user, afs_pass))
+
+    # convert to JSON
+    response = put_response.json()
+
+    # send them to the template
+    context = {
+        "response": response,
+        "session_id": session_id,
+    }
+
+    return render_template("payment.html", **context)
 
 
 if __name__ == "__main__":
