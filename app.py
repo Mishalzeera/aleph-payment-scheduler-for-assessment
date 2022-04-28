eba952218e6d947fb24aacc6e5c1eaa12e395c90
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-04-28 12:39:50 +0200

    Alternative HTML JS only approach from AFS.

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ app.secret_key = os.environ.get("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+@app.route('/alt-payment')
+def alt_payment():
+    return render_template("alt_payment.html")
+
 @app.route('/payment')
 def payment():
 
@@ -65,6 +69,48 @@ def payment():
     }
 
     return render_template("payment.html", **context)
+
+
+# @app.route('/update_alt')
+# def update_alt():
+#     # auth environment variables
+#     afs_user = os.environ.get("AFS_USER")
+#     afs_pass = os.environ.get("AFS_PASS")
+
+#     attempt_num = 1
+
+#     body = {"apiOperation": "PAY",
+# 	"authentication":{
+# 		"transactionId": "TxnID_" + attempt_num
+# 	},
+# 	"order": {
+# 		"amount": 1.00,
+# 		"currency": "SAR",
+# 		"reference": "OrdRef_" + attempt_num
+# 	},
+# 	"transaction": {
+# 		"reference": "TrxRef_" + attempt_num
+#     	},
+# 	"session": {
+# 		"id": "{{SessionId}}"
+# 	},
+# 	"sourceOfFunds": {
+# 		"type": "CARD"
+# 	}
+# }
+
+#     payload = json.dumps(body)
+
+#     url = f'https://afs.gateway.mastercard.com/api/rest/version/63/merchant/TEST100065243/order/OrdID_{attempt_num}/transaction/1'
+#     try:
+#         res = requests.put(url, data=payload, auth=(afs_user, afs_pass))
+#         print(res.json())
+#     except:
+#         if attempt_num < 5:
+#             attempt_num += 1 
+#             res = requests.put(url, data=body, auth=(afs_user, afs_pass))
+#         else:
+#             return  
 
 
 if __name__ == "__main__":
