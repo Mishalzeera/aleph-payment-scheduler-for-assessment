9897359323ee3ee90145facd122db8ede62c1fe0
Salman <47951782+ssairafi@users.noreply.github.com>
2022-05-07 11:51:09 +0300

    fixes and support for server and client side 3ds

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from time import process_time_ns
-from turtle import setundobuffer
+from turtle import goto, setundobuffer
 import requests
 import json
 import secrets
@@ -20,8 +20,8 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/payment')
-def payment():
+@app.route('/carddetails')
+def carddetails():
 
     # auth environment variables
     afs_user = os.environ.get("AFS_USER")
@@ -31,7 +31,9 @@ def payment():
 
     print("------------------------- init afs session ----------------------------")
 
-    # URL to post initial request to
+    # mastercard session is a json object that is continuously updated by subsequent transactions
+
+    # URL to post initial request to create a session
     post_url = f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/session'
 
     # JSON payload to send in POST request body
@@ -80,10 +82,10 @@ def payment():
     })
     put_response = requests.put(put_url, data=put_payload, auth=(afs_user, afs_pass)).json()
 
-    # confirm order details by extracting from put_response - if this is fine we can remove the order details from the local cookie
-    print("order details from put response")
-    print("id", put_response["order"]["id"])
-    print("amount", put_response["order"]["currency"], put_response["order"]["amount"])
+    # # confirm order details by extracting from put_response - if this is fine we can remove the order details from the local cookie
+    # print("order details from put response")
+    # print("id", put_response['order']['id'])
+    # print("amount", put_response['order']['currency'], put_response['order']['amount'])
 
     # send them to the template 
     context = {
@@ -93,10 +95,10 @@ def payment():
         "merchant_id": afs_url
     }
 
-    return render_template("payment.html", **context)
+    return render_template("carddetails.html", **context)
 
-@app.route('/confirm')
-def confirm():
+@app.route('/authenticate')
+def authenticate():
   
     # auth environment variables
     afs_user = os.environ.get("AFS_USER")
@@ -110,36 +112,49 @@ def confirm():
     order_amount = session['order_amount'] # can remove this once i'm done to help clean up
     order_currency = session['order_currency'] # can remove this once i'm done to help clean up
 
-    print("order details from cookie:")
-    print("id", order_id)
-    print("amount", order_currency, order_amount)
+    # ONLY USEFUL FOR THREEDS JS API LIBRARY - CAN BUNDLE INTO SINGLE REQUEST FOR 3DS SERVER API
 
-    print("------------------------ begin confirming payment ------------------")
+    # print("----------------------- updating session with auth details -------------------------")
+    
+    # # update session with authentication details
+    # put_url = f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/session/{session_id}'
+    # put_payload = json.dumps({
+    #     "authentication": {
+    #         "acceptVersions": "3DS1,3DS2",
+    #         "channel": "PAYER_BROWSER",
+    #         "purpose": "PAYMENT_TRANSACTION",
+    #         "redirectResponseUrl":"http://127.0.0.1:5500/payment"
+    #     },
+    #     # "session": {
+    #     #     "id": session_id
+    #     # },
+    #     # "order":{
+    #     #     "currency": order_currency,
+    #     #     "reference": order_id
+    #     # }
+    #     # ,
+    #     # "transaction":{
+    #     #     "reference": transaction_id_3ds #this must reference t
+    #     # }
+    # })
+    # put_res = requests.put(put_url, data=put_payload, auth=(afs_user, afs_pass)).json()
 
-    print("session id is", session_id)
+    # put_response = json.dumps(put_res,indent=4,sort_keys=True)
 
-    # now time to generate some transaction ids based on need:
+    # print(put_response)
 
-    transaction_id_3ds = secrets.token_hex(8)
-    print("transaction id for 3ds is", transaction_id_3ds)
-
-    transaction_id_3ds_auth = secrets.token_hex(8)
-    print("transaction id for 3ds auth is", transaction_id_3ds_auth)
-
-    transaction_id_pay = secrets.token_hex(8)
-    print("transaction id for payment is", transaction_id_pay)
-
-    auth_token = ""
-    auth_available = True
-
-#     # 3DS AUTH BEGINS
+    # 3DS AUTH BEGINS
 
     print("----------------------- init 3DS -------------------------")
     
-# comment from mastercard website:
-# As soon as you have the card number, invoke the Initiate Authentication operation with the payment session identifier. It is recommended that you perform this asynchronously, so that the payer can continue filling out payment details.
-# recommended flow: https://afs.gateway.mastercard.com/api/documentation/apiDocumentation/threeds/version/57/api.html?locale=en_US
+    # comment from mastercard website:
+    # As soon as you have the card number, invoke the Initiate Authentication operation with the payment session identifier. It is recommended that you perform this asynchronously, so that the payer can continue filling out payment details.
+    # recommended flow: https://afs.gateway.mastercard.com/api/documentation/apiDocumentation/threeds/version/57/api.html?locale=en_US
 
+    # init 3ds transaction - the same transaction number is used for both INITITATE_AUTHENTICATION and AUTHENTICATE_PAYER
+
+    transaction_id_3ds = secrets.token_hex(8)
+    print("transaction id for 3ds is", transaction_id_3ds)
 
     init_3ds_url = f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/order/{order_id}/transaction/{transaction_id_3ds}'
 
@@ -150,14 +165,14 @@ def confirm():
             "channel": "PAYER_BROWSER",
             "purpose": "PAYMENT_TRANSACTION"
         },
-        "correlationId": "test",
+        # "correlationId": "test",
         "session": {
             "id": session_id
-        },
-        "order":{
-            "currency": order_currency,
-            "reference": order_id
         }
+        # "order":{
+        #     "currency": order_currency,
+        #     "reference": order_id
+        # }
         # ,
         # "transaction":{
         #     "reference": transaction_id_3ds #this must reference t
@@ -165,60 +180,161 @@ def confirm():
 	})
         
     init_3ds_res = requests.put(init_3ds_url, data=init_3ds_payload, auth=(afs_user, afs_pass)).json()
-    print(json.dumps(init_3ds_res,indent=4,sort_keys=True))
-	
-    # auth_available = init_3ds_response["transaction"]["authenticationStatus"] == "AUTHENTICATION_AVAILABLE"
-    
-    print("authentication available", auth_available)
 
-    # only request authentication if available to reduce calls
-    if auth_available: 
-        print("----------------------- 3DS authentication -------------------------")
+    init_3ds_response = json.dumps(init_3ds_res,indent=4,sort_keys=True)
 
-        auth_3ds_url = 	f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/order/{order_id}/transaction/{transaction_id_3ds_auth}'
+    print(init_3ds_response)
 
-        auth_3ds_payload = json.dumps({
-            "apiOperation": "AUTHENTICATE_PAYER",
-            "authentication":{
-                "redirectResponseUrl":	"https://google.com" # server page to redirect back to once the 3ds process is done?
+    # if result is failed, card is most likely not enrolled in any 3DS schene, so update context with error message and render
+    if init_3ds_res['result'] != "SUCCESS":
+        context = {
+            "response": "Gateway reponse is " + init_3ds_res['response']['gatewayCode'],
+            "session_id": session_id,
+            "afs_version": afs_version,
+            "merchant_id:": afs_url
+        }
+        return render_template("authenticate.html", **context)
+
+    # if above doesn't catch, assume it's a SUCCESS
+
+    print("----------------------- 3DS authentication -------------------------")
+
+    # To increase the likelihood of the authentication being successful, provide as much information about the payer and the transaction as possible.
+    #
+    # If the information in the request is sufficient to allow the authentication scheme to confirm the payer's identity the response will include the authentication data (frictionless flow).
+    # Alternatively (challenge flow), it may be necessary for the payer to interact with the authentication scheme to confirm their identity (e.g. by providing a one-time password sent to them by their card issuer).
+    # In this case the response will contain an HTML excerpt that you must inject into your page.
+    # This will establish the interaction between the payer and the authentication scheme.
+    # After authentication has been completed the payer will be redirected back to your website using the URL provided by you in field authentication.redirectResponseUrl in the Authenticate Payer request.
+
+    # requires device information to generate the browser-sepcific authentication UI code
+
+    # generate transaction id for authentication
+
+    transaction_id_3ds_auth = transaction_id_3ds # secrets.token_hex(8) - must be the same order id and transaction id as the init transaction
+    print("transaction id for 3ds auth is", transaction_id_3ds_auth)
+
+    callback_url = "http://127.0.0.1:5500/payment" #hardcoded for now
+
+    print("callback URL is ", callback_url)
+
+    auth_3ds_url = 	f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/order/{order_id}/transaction/{transaction_id_3ds_auth}'
+
+    auth_3ds_payload = json.dumps({
+        "apiOperation": "AUTHENTICATE_PAYER",
+        "authentication":{
+            "redirectResponseUrl":	callback_url # callback doesn't seem to work - the generated HTML doesn't seem to go anywhere...
+            # ,"channel": "PAYER_BROWSER" -------- seems to be the default channel
+        },
+
+        "device": {
+            "browser": "MOZILLA",# hardcoded - need to extract user-agent from browser
+            "browserDetails": {
+                "3DSecureChallengeWindowSize": "FULL_SCREEN",# hardcoded - need to extract from browser
+                "acceptHeaders": "application/json",
+                "colorDepth": 24,# hardcoded - need to extract from browser
+                "javaEnabled": True,# hardcoded - need to extract from browser
+                "language": "en-US",
+                "screenHeight": 640,# hardcoded - need to style the div in the template
+                "screenWidth": 480,# hardcoded - need to style the div in the template
+                "timeZone": 273 # hardcoded - need to extract from browser
             },
+            "ipAddress": "127.0.0.1"
+        },
 
-            "order": {
-                "amount": order_amount,
-                "currency": order_currency
-            },
+        "order": {
+            "amount": order_amount,
+            "currency": order_currency
+        },
 
-            "session": {
-                "id": session_id
-            }
-        })
+        "session": {
+            "id": session_id
+        }
+    })
 
-        auth_3ds_res = requests.put(auth_3ds_url, auth=(afs_user, afs_pass), data=auth_3ds_payload).json()
-        print(json.dumps(auth_3ds_res,indent=4,sort_keys=True))
-	
-        # auth_token = auth_3ds_response["authentication"]["3ds"]["authenticationToken"]
-        # print("auth token", auth_token)
-        # print(auth_3ds_response)
+    auth_3ds_res = requests.put(auth_3ds_url, auth=(afs_user, afs_pass), data=auth_3ds_payload).json()
 
-#     3DS AUTH ENDS
-    
+    auth_3ds_response = json.dumps(auth_3ds_res,indent=4,sort_keys=True)
+
+    print(auth_3ds_response)
+
+   # if result is PENDING, the next step is to display the 3DS interaction for authententication, otherwise, bomb out
+
+    if auth_3ds_res['result'] != "PENDING":
+        print("-------------------- AUTH FAILED ----------------")
+
+        context = {
+            "response": "Gateway reponse is " + auth_3ds_res['response']['gatewayCode'],
+            "session_id": session_id,
+            "afs_version": afs_version,
+            "merchant_id:": afs_url
+        }
+        return render_template("authenticate.html", **context)
+
+    auth_interaction = auth_3ds_res['authentication']['redirectHtml']
+
+    print("------------------------ INJECTING AUTH CODE --------------------")
+    print(auth_interaction)
+    print("-----------------------------------------------------------------")
+
+    # send code to the template 
+
+    context = {
+
+        # "response": auth_3ds_response,
+        "session_id": session_id,
+        "afs_version": afs_version,
+        "merchant_id": afs_url,
+        "order_id": order_id,
+        "transaction_id": transaction_id_3ds,
+        "auth_interaction": auth_interaction
+    }
+
+    return render_template("authenticate.html", **context)
+
+@app.route('/payment')
+def payment():
+
+    # auth environment variables
+    afs_user = os.environ.get("AFS_USER")
+    afs_pass = os.environ.get("AFS_PASS")
+    afs_url = os.environ.get("AFS_URL")
+    afs_version = os.environ.get("AFS_VERSION")
+
+    # extract variables from session cookies
+    session_id = session['afs_session_id']
+    order_id = session['order_id']
+    order_amount = session['order_amount'] # can remove this once i'm done to help clean up
+    order_currency = session['order_currency'] # can remove this once i'm done to help clean up
+
+    print("session id is", session_id)
+
+    print("order details from cookie:")
+    print("id", order_id)
+    print("amount", order_currency, order_amount)
+
+    # do payment
+
+    transaction_id_pay = secrets.token_hex(8)
+    print("transaction id for payment is", transaction_id_pay)
+
     print("----------------------- initiating payment ------------------------")
 
     pay_url = f'https://afs.gateway.mastercard.com/api/rest/version/{afs_version}/merchant/{afs_url}/order/{order_id}/transaction/{transaction_id_pay}'
 
     payload_dict = {
         "apiOperation": "PAY",
-        "authentication": {
-            "transactionId": transaction_id_3ds_auth
-        },
+        # "authentication": {
+        #     "transactionId": transaction_id_3ds_auth
+        # },
         "order": {
             "amount": order_amount,
             "currency": order_currency,
             "reference": order_id
         },
-        "transaction": {
-            "reference": transaction_id_pay
-        },
+        # "transaction": {
+        #     "reference": transaction_id_pay
+        # },
         "session": {
             "id": session_id
         },
@@ -228,29 +344,22 @@ def confirm():
         }
     }
 
-    # if auth_available:
-    #     payload_dict['authentication'] = {
-    #         "3ds":{
-    #             "authenticationToken": auth_token
-    #         }
-    #     }
-
     payload = json.dumps(payload_dict)
 
-    response = requests.put(pay_url, data=payload, auth=(afs_user, afs_pass)).json()
+    pay_res = requests.put(pay_url, data=payload, auth=(afs_user, afs_pass)).json()
+
+    pay_response = json.dumps(pay_res,indent=4,sort_keys=True)
+
+    print(pay_response)
 
     context = {
-        "response": response,
+        "response": pay_response,
         "session_id": session_id,
         "afs_version": afs_version,
         "merchant_id:": afs_url
     }
 
-    print(json.dumps(response,indent=4,sort_keys=True))
-
-    # confirmation page is now actually a payment page
-
-    return render_template("confirm.html", **context)
+    return render_template("payment.html", **context)
 
 
 if __name__ == "__main__":
